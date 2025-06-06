9acd21fda376e1a2a197cb3d8ba6e6ce1f3f8bfc
DINESH D <146455353+DineshDesappan@users.noreply.github.com>
2025-06-06 10:07:40 +0530

    Updated Some Files

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProject from "./components/NoProject";
 import SideBar from "./components/SideBar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectState, setProjectState] = useState({
     projectId: undefined,
     project: [],
+    task: [],
   });
 
   function handleCreateProject() {
@@ -18,16 +20,23 @@ function App() {
     });
   }
 
-
-function handleCancelProject(){
-   setProjectState((prevProject) => {
+  function handleSelectedProject(projectId) {
+    setProjectState((prevProject) => {
+      return {
+        ...prevProject,
+        projectId: projectId,
+      };
+    });
+  }
+  function handleCancelProject() {
+    setProjectState((prevProject) => {
       return {
         ...prevProject,
         projectId: undefined,
       };
     });
-}  
-function handleAddProject(projectData) {
+  }
+  function handleAddProject(projectData) {
     setProjectState((prevProject) => {
       const newProject = {
         ...projectData,
@@ -35,22 +44,74 @@ function handleAddProject(projectData) {
       };
       return {
         ...prevProject,
-        projectId:undefined,
+        projectId: undefined,
         project: [...projectState.project, newProject],
       };
     });
   }
 
-  let content;
+  function handleDeleteProject() {
+    setProjectState((prevProject) => {
+      return {
+        ...prevProject,
+        projectId: undefined,
+        project: prevProject.project.filter(
+          (project) => project.id !== prevProject.projectId
+        ),
+      };
+    });
+  }
+
+  function addTask(text) {
+    setProjectState((prevProject) => {
+      const newTask = {
+        text: text,
+        projectId: prevProject.projectId,
+        id: Math.random(),
+      };
+      return {
+        ...prevProject,
+        task: [...prevProject.task, newTask],
+      };
+    });
+  }
+  function deleteTask(id) {
+    setProjectState((prevProject) => {
+      return {
+        ...prevProject,
+        task: prevProject.task.filter((task) => task.id !== id),
+      };
+    });
+  }
+
+  const selectedProjectId = projectState.project.find(
+    (project) => project.id === projectState.projectId
+  );
+  let content = (
+    <SelectedProject
+      onAddTask={addTask}
+      onDeleteTask={deleteTask}
+      onDelete={handleDeleteProject}
+      project={selectedProjectId}
+      tasks={projectState.task}
+    />
+  );
   if (projectState.projectId === null) {
-    content = <NewProject onCancel={handleCancelProject} onAdd={handleAddProject}/>;
+    content = (
+      <NewProject onCancel={handleCancelProject} onAdd={handleAddProject} />
+    );
   } else if (projectState.projectId === undefined) {
     content = <NoProject onCreate={handleCreateProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar projects={projectState.project} oncreate={handleCreateProject} />
+      <SideBar
+        selectedProjectId={projectState.projectId}
+        onSelectedProject={handleSelectedProject}
+        projects={projectState.project}
+        oncreate={handleCreateProject}
+      />
       {/* <NewProject/> */}
       {content}
     </main>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./Button";
 import { list } from "postcss";
 
-function SideBar({ oncreate, projects }) {
+function SideBar({ oncreate, projects, onSelectedProject, selectedProjectId }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-black text-stone-50 rounded-r-xl md:w-72">
       <h2 className="mb-8 font-bold md:text-xl text-stone-200 uppercase">
@@ -12,11 +12,26 @@ function SideBar({ oncreate, projects }) {
         <Button onClick={oncreate}> + Add Project</Button>
       </div>
       <ul className="my-8">
-        {projects.map((project) => (
-          <li key={project.id}>
-            <button className="w-full px-2 py-1 text-left text-stone-400 hover:text-stone-200 hover:bg-stone-800 rounded-sm my-1">{project.title}</button>
-          </li>
-        ))}
+        {projects.map((project) => {
+          let cssClasses = "w-full px-2 py-1 text-left text-stone-400 hover:text-stone-200 hover:bg-stone-800 rounded-sm my-1";
+
+          if (project.id === selectedProjectId) {
+            cssClasses += " bg-stone-700 text-stone-400";
+          }else {
+            cssClasses += " bg-stone-950 ";
+          }
+
+          return (
+            <li key={project.id}>
+              <button
+                onClick={() => onSelectedProject(project.id)}
+                className={cssClasses}
+              >
+                {project.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
